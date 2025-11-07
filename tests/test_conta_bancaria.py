2b185bcf5d13dbc05a28c0e48d1921a521d831f9
JESSE BRENDON GOMES FARIAS EUSEBIO <jesse.eusebio@maisunifacisa.com.br>
2025-11-07 19:23:56 -0300

    test: cria teste para saldo inicial zero (RED)

---
+++ tests/test_conta_bancaria.py
@@ -0,0 +1,9 @@
+﻿import sys, pathlib
+sys.path.append(str(pathlib.Path(__file__).resolve().parents[1] / 'src'))
+
+import pytest
+from conta_bancaria import ContaBancaria
+
+def test_saldo_inicial_zero():
+    conta = ContaBancaria()
+    assert conta.saldo == 0
