47d27e364c76e978597de48c48bf46b638309cd4
JESSE BRENDON GOMES FARIAS EUSEBIO <jesse.eusebio@maisunifacisa.com.br>
2025-11-07 19:24:59 -0300

    test: adiciona teste para depósito válido (RED)

--- tests/test_conta_bancaria.py
+++ tests/test_conta_bancaria.py
@@ -5,3 +5,8 @@ from src.conta_bancaria import ContaBancaria
 def test_saldo_inicial_zero():
     conta = ContaBancaria()
     assert conta.saldo == 0
+
+def test_deposito_aumenta_saldo():
+    conta = ContaBancaria()
+    conta.depositar(100)
+    assert conta.saldo == 100
